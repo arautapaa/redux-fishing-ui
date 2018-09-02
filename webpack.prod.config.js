b595eb370c39268a632cab76fdd6de7801766dda
Antti Rautapaa <antti.rautapaa@gmail.com>
2018-09-02 15:19:35 +0300

    Adding Travis support

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,13 @@
 var webpack = require("webpack");
+var path = require('path');
+var fs = require('fs');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var outputDir = __dirname + "/dist";
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = require('./webpack.config.js');    // inherit from the main config file
 
 // disable the hot reload
@@ -7,6 +15,8 @@ module.exports.entry = [
   'babel-polyfill',
   __dirname + '/' + module.exports.app_root + '/index.js'
 ];
+
+
 
 // production env
 module.exports.plugins.push(
@@ -16,6 +26,13 @@ module.exports.plugins.push(
     }
   })
 );
+
+
+module.exports.output= {
+    path: __dirname + '/dist',
+    publicPath: '',
+    filename: 'js/bundle.[hash].js',
+},
 
 // compress the js file
 module.exports.plugins.push(
@@ -27,12 +44,60 @@ module.exports.plugins.push(
   })
 );
 
+module.exports.plugins.push(new HtmlWebpackPlugin({
+    title: 'QA Draught test application',
+    // Load a custom template (lodash by default see the FAQ for details)
+    template: 'public/index.html'
+  }));
+
+module.exports.plugins.push(new CleanWebpackPlugin(['dist'])),
+
 // export css to a separate file
-module.exports.module.loaders[1] = {
+module.exports.module.loaders[2] = {
   test: /\.scss$/,
   loader: ExtractTextPlugin.extract('css!sass'),
 };
 
 module.exports.plugins.push(
-  new ExtractTextPlugin('../css/main.css')
+  new ExtractTextPlugin('css/main.[hash].css')
 );
+
+module.exports.plugins.push(new CopyWebpackPlugin([{
+  from : __dirname + '/public/media', to : "media/"
+}]));
+
+// Hashing of this kind happens only in prod.
+module.exports.plugins.push(
+  // To rewrite stuff like `bundle.js` to `bundle-[hash].js` in files that refer to it, I tried and
+  // didn't like the following plugin: https://github.com/ampedandwired/html-webpack-plugin
+  // for 2 reasons:
+  //    1. because it didn't work with HMR dev mode...
+  //    2. because it's centered around HTML files but I also change other files...
+  // I hope we can soon find something standard instead of the following hand-coding.
+  function () {
+    this.plugin("done", function (stats) {
+        var replaceInFile = function (filePath, toReplace, replacement) {
+            var replacer = function (match) {
+                console.log('Replacing in %s: %s => %s', filePath, match, replacement);
+                return replacement
+            };
+            var str = fs.readFileSync(filePath, 'utf8');
+            var out = str.replace(new RegExp(toReplace, 'g'), replacer);
+            fs.writeFileSync(filePath, out);
+        };
+
+        var hash = stats.hash; // Build's hash, found in `stats` since build lifecycle is done.
+
+        replaceInFile(path.join(outputDir, 'index.html'),
+            'bundle.js',
+            'bundle.' + hash + '.js'
+        );
+
+        replaceInFile(path.join(outputDir, 'index.html'),
+            'main.css',
+            'main.' + hash + '.css'
+        );
+    });
+  }
+);
+
